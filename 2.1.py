27d7f71283f11f65c11463819bc1106c197329af
lgl90pro <60923397+lgl90pro@users.noreply.github.com>
2020-02-13 20:28:07 +0200

    Add files via upload

---
+++ 2.1.py
@@ -0,0 +1,5 @@
+import math
+a, b = int(input('Введіть а: ')), int(input('Введіть b: '))
+ser_arifm = (a+b)/2
+ser_geom = math.sqrt(a*b)
+print(f'Середнє арифметичне: {ser_arifm}\nСереднє геометричне: {ser_geom}')
